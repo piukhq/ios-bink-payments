5986844000f683158f82650c8387a91d41506f7a
Ricardo Silva <risilva10223@gmail.com>
2022-09-13 12:09:42 +0100

    Made class public

--- Sources/BinkPayments/BinkPaymentsManager.swift
+++ Sources/BinkPayments/BinkPaymentsManager.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-class BinkPaymentsManager {
-    static let shared = BinkPaymentsManager()
+public class BinkPaymentsManager {
+    public static let shared = BinkPaymentsManager()
     
     private var token: String!
     private var environmentKey: String!
 
     private init() {}
     
-    func configure(token: String!, environmentKey: String!) {
+    public func configure(token: String!, environmentKey: String!) {
         assert(!token.isEmpty && !environmentKey.isEmpty, "Bink Payments SDK Error - Not Initialised due to missing token/environment key")
         
         self.token = token
