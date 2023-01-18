c0dd27ea388b6b3ce6f8ec1cfeecef691bdeedb3
Sean Williams <swilliams@bink.com>
2023-01-18 15:56:08 +0000

    Create add payment card button

--- Test-DK/Test-DK/ContentView.swift
+++ Test-DK/Test-DK/ContentView.swift
@@ -6,14 +6,18 @@
 //
 
 import SwiftUI
+import BinkPayments
 
 struct ContentView: View {
+    let viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                viewModel.paymentsManager.launchScanner(delegate: viewModel)
+            } label: {
+                Text("Add Payment Card")
+            }
         }
         .padding()
     }
@@ -23,4 +27,30 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+class ViewModel {
+    let paymentsManager = BinkPaymentsManager.shared
+
+    init() {
+        let config = Configuration(testLoyaltyPlanID: "203", productionLoyaltyPlanID: "203", trustedCredentialType: .add)
+        
+        paymentsManager.configure(
+            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6ImFjY2Vzcy1zZWNyZXQtMiJ9.eyJzdWIiOjM4MjgzLCJjaGFubmVsIjoiY29tLmJpbmsud2FsbGV0IiwiaWF0IjoxNjUxMTUyOTU5LCJleHAiOjE2ODI2ODg5NTl9.mvcKT3eALLCOENFIWl39Zo6t5Jux8RVuMH0-nawnjNPjv5tGALlpM6-gNcPtdXEB6_ZL_uJAmaJZNT4h1V-yYw",
+            environmentKey: "1Lf7DiKgkcx5Anw7QxWdDxaKtTa",
+            configuration: config,
+            isDebug: true)
+    }
+}
+
+extension ViewModel: BinkScannerViewControllerDelegate {
+    func binkScannerViewControllerShouldEnterManually(_ viewController: BinkPayments.BinkScannerViewController, completion: (() -> Void)?) {
+        paymentsManager.launchAddPaymentCardScreen()
+    }
+    
+    func binkScannerViewController(_ viewController: BinkPayments.BinkScannerViewController, didScan paymentCard: BinkPayments.PaymentAccountCreateModel) {
+        print("Did scan")
+    }
+    
+    
 }
