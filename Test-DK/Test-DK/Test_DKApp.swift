3288fbc2664651340ba522a233f30bb914b4fac9
Sean Williams <swilliams@bink.com>
2023-01-18 09:20:32 +0000

    Add test app

---
+++ Test-DK/Test-DK/Test_DKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_DKApp.swift
+//  Test-DK
+//
+//  Created by Sean Williams on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Test_DKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
