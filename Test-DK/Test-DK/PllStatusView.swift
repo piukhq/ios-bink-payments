b3d9dcbb07544b38c1ef94c25615c01e7cd7a9df
Sean Williams <swilliams@bink.com>
2023-01-20 11:40:52 +0000

    Setup payment cards view

--- Test-DK/Test-DK/PllStatusView.swift
+++ Test-DK/Test-DK/PllStatusView.swift
@@ -19,7 +19,7 @@ struct PllStatusView: View {
     
     var body: some View {
         VStack {
-            BinkButton(text: "Loyalty Card") {
+            BinkButton(text: "Pll Status for Loyalty Card") {
                 statusType = .loyalty
                 viewModel.linkedPaymentCardsForLoyaltyCard()
             }
@@ -29,13 +29,35 @@ struct PllStatusView: View {
             }
             
             Spacer()
-            
-            if viewModel.showLinkedPaymentCards {
-                VStack {
-                    ForEach(viewModel.loyaltyCardPllState.linked, id: \.apiId) { paymentAccount in
-                        Text(paymentAccount.nameOnCard ?? "")
-                    }
+
+            if let _ = viewModel.loyaltyCardPllState {
+                ScrollView {
+                    Text("Last time checked:")
+                        .font(.headline)
+                    Text(viewModel.loyaltyCardPllState.timeChecked?.formatted(date: .complete, time: .standard) ?? "")
                     
+                    Spacer()
+                    
+                    /// Linked Cards
+                    Text("LINKED")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
+                    
+                    PaymentCardsView(status: .linked, pllState: viewModel.loyaltyCardPllState)
+
+                    
+                    Rectangle()
+                        .frame(height: 2)
+                        .foregroundColor(.pink)
+                    
+                    /// Unlinked Cards
+                    Text("UNLINKED")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                    
+                    PaymentCardsView(status: .unlinked, pllState: viewModel.loyaltyCardPllState)
                 }
             }
         }
@@ -49,11 +71,45 @@ struct PllStatusView_Previews: PreviewProvider {
     }
 }
 
+struct PaymentCardsView: View {
+    enum LinkedStatus {
+        case linked
+        case unlinked
+    }
+    
+    var status: LinkedStatus
+    var pllState: LoyaltyCardPLLState
+    
+    var paymentCardsForLinkedStatus: [PaymentAccountResponseModel] {
+        switch status {
+        case .linked:
+            return pllState.linked
+        case .unlinked:
+            return pllState.unlinked
+        }
+    }
+    
+    var body: some View {
+        ForEach(paymentCardsForLinkedStatus, id: \.apiId) { paymentAccount in
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("ID: \(String(paymentAccount.apiId ?? 0))")
+                        .fontWeight(.medium)
+                        .foregroundColor(status == .linked ? .green : .red)
+                    Text(paymentAccount.nameOnCard ?? "")
+                        .fontWeight(.light)
+                }
+                Spacer()
+                Text("Card number ending: \(paymentAccount.lastFour ?? "")")
+            }
+            .padding(.bottom, 10)
+        }
+    }
+}
+
 class PllStatusViewModel: ObservableObject {
     private let paymentManager: BinkPaymentsManager
-    var loyaltyCardPllState: LoyaltyCardPLLState!
-    
-    @Published var showLinkedPaymentCards = false
+    @Published var loyaltyCardPllState: LoyaltyCardPLLState!
 
     init(paymentManager: BinkPaymentsManager) {
         self.paymentManager = paymentManager
@@ -61,11 +117,9 @@ class PllStatusViewModel: ObservableObject {
     
     func linkedPaymentCardsForLoyaltyCard() {
         if let loyaltyCard = paymentManager.loyaltyCard(from: 237193) {
-            loyaltyCardPllState = paymentManager.pllStatus(for: loyaltyCard) { pllState in
-                
+            loyaltyCardPllState = paymentManager.pllStatus(for: loyaltyCard) { [weak self] pllState in
+                self?.loyaltyCardPllState = pllState
             }
-            
-            showLinkedPaymentCards = true
         }
     }
 }
